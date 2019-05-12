c974ef18cf98870d721549f056efc35c85bca006
drzhang <654014730@qq.com>
2019-05-12 15:28:04 +0800

    main文件

---
+++ master/main/master.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/drzhangg/go-crontab/master"
+	"runtime"
+	"time"
+)
+
+var (
+	confFile string //配置文件路径
+)
+
+//解析命令行参数
+func initArgs() {
+	//master -config ./master.json -xxx 123 -yyy ddd
+	//master -h
+	flag.StringVar(&confFile, "config", "./master.json", "指定master.json")
+	flag.Parse()
+}
+
+func initEnv() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+	var (
+		err error
+	)
+
+	//初始化命令行参数
+	initArgs()
+
+	//初始化线程
+	initEnv()
+
+	//加载配置
+	if err = master.InitConfig(confFile); err != nil {
+		goto ERR
+	}
+
+	//加载etcd配置
+	if err = master.InitJobMgr(); err != nil {
+		goto ERR
+	}
+
+	//启动API HTTP服务
+	if err = master.InitApiServer(); err != nil {
+		goto ERR
+	}
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+
+	//正常退出
+	return
+
+ERR:
+	fmt.Println(err)
+}
